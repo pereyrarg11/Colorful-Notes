50b3750746e42b1ed9a8640c9edf16b423f3c06c
Gabriel Pereyra <pereyrarg11@gmail.com>
2023-06-06 13:02:01 -0600

    chore: screen for add-edit-note

---
+++ app/src/main/java/com/pereyrarg11/colorfulnotes/feature_note/presentation/add_edit_note/components/TransparentHintTextField.kt
@@ -0,0 +1,39 @@
+package com.pereyrarg11.colorfulnotes.feature_note.presentation.add_edit_note.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusState
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+
+@Composable
+fun TransparentHintTextField(
+    text: String,
+    hint: String,
+    onValueChange: (String) -> Unit,
+    onFocusChange: (FocusState) -> Unit,
+    modifier: Modifier = Modifier,
+    isHintVisible: Boolean = true,
+    textStyle: TextStyle = TextStyle(),
+    singleLine: Boolean = false,
+) {
+    Box(modifier = modifier) {
+        BasicTextField(
+            value = text,
+            onValueChange = onValueChange,
+            singleLine = singleLine,
+            textStyle = textStyle,
+            modifier = Modifier
+                .fillMaxWidth()
+                .onFocusChanged { onFocusChange(it) }
+        )
+        if (isHintVisible) {
+            Text(text = hint, style = textStyle, color = Color.DarkGray)
+        }
+    }
+}
